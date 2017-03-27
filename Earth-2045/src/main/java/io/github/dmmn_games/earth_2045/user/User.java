79dc6cfad48d3f41777d82e87e678e462aa2b416
BuildTools <unconfigured@null.spigotmc.org>
2017-03-27 19:25:51 +0300

    Some methods and variables

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/user/User.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/user/User.java
@@ -3,12 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gr.teicm.informatics.escape_dungeon.user;
+package io.github.dmmn_games.earth_2045.user;
+
+import io.github.dmmn_games.earth_2045.tools.ITool;
 
 /**
  *
  * @author iordkost
  */
 public class User {
+
+    private String username;
+    public int room;
+    private int pos;
+    private int health;
+    private int score;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public void pick_up(ITool tool) {
+
+    }
+
+    public void leave(ITool tool) {
+    }
+    //mallon klasi domatiou giauto kai sxolio 
+
+    /* public void changeRoom(/*klasi domatiou){
+    int numbRoom;
     
+}*/
+
+    public void go(//kateuthinsi klasis 
+            ) {
+        int position;
+        position = 0;
+        String direction;
+
+    }
+
+    public void healthBar() {
+        int lifes = 5;
+        for (int i = 0; i <= lifes; i++) {
+            //if(){life-=life;}
+
+        }
+    }
+    public void score()
+    {}
 }
