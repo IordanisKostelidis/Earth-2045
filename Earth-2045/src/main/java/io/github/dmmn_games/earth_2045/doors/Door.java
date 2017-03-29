561ae81dddbfd867c570a35bcf8f7d26d53bfd3b
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-03-29 12:27:17 +0300

    modified  door class

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/doors/Door.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/doors/Door.java
@@ -3,12 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gr.teicm.informatics.escape_dungeon.doors;
+package io.github.dmmn_games.earth_2045.doors;
 
 /**
  *
  * @author iordkost
  */
 public class Door {
+    int currentDoor;
+    int nextDoor;
+    int IdPass;
+    boolean isOpen;
+
+    public Door(int currentDoor, int nextDoor, int IdPass, boolean isOpen) {
+        this.currentDoor = currentDoor;
+        this.nextDoor = nextDoor;
+        this.IdPass = IdPass;
+        this.isOpen = isOpen;
+    }
+
+    public int getCurrentDoor() {
+        return currentDoor;
+    }
+
+    public int getNextDoor() {
+        return nextDoor;
+    }
+
+    public int getIdPass() {
+        return IdPass;
+    }
+
+    public boolean isIsOpen() {
+        return isOpen;
+    }
+    
     
 }
