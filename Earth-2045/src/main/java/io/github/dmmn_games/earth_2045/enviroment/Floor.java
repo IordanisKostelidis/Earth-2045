d20683abe12a49cd32898afc69eeb825fccd267e
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-03-29 12:19:26 +0300

    Created the Object,item and Room Classes with the basic data members and thebasic constructors and getters

---
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Floor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.dmmn_games.earth_2045.enviroment;
+
+import java.util.*;
+
+/**
+ *
+ * @author makis
+ */
+public class Floor {
+    private ArrayList<Room> floors = new ArrayList<Room>();
+    
+    
+}
---
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Room.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.dmmn_games.earth_2045.enviroment;
+
+import io.github.dmmn_games.earth_2045.items.*;
+import io.github.dmmn_games.earth_2045.tools.*;
+import java.util.*;
+
+/**
+ *
+ * @author makis
+ */
+public class Room {
+    
+    private ArrayList<ITool> tools= new ArrayList<ITool>();
+    private ArrayList<IItem> items= new ArrayList<IItem>();
+    
+    
+    
+}
