f4df9e4a01fea5437ff0e6516f2c98e23b8d104b
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-17 15:51:34 +0300

    change data-members and methods in class Floor and room

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Floor.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Floor.java
@@ -18,98 +18,21 @@ import java.util.*;
  */
 public class Floor implements Serializable {
 
-    private List<Room> Rooms = new ArrayList<>();
-    private List<Door> Doors = new ArrayList<>();
-    private List<Bot> Bots = new ArrayList<>();
+    private List<Room> Rooms = new ArrayList<>();    
 
     public Floor() {
         this.Rooms = new ArrayList<>();
-        this.Doors = new ArrayList<>();
-        this.Bots = new ArrayList<>();
-    }
-
-    public void addRoom(Room newRoom) {
-        Rooms.add(newRoom);
     }
 
     public Room getRoom(int index) {
         return Rooms.get(index);
     }
-
+    
+    /*public void addRoom(Room newRoom) {
+        newRoom.Room.add(newRoom);
+    }*/
+    
     public List<Room> getRooms() {
         return this.Rooms;
     }
-
-    public void addDoor(Door newDoor) {
-        Doors.add(newDoor);
-    }
-
-    public Door getDoor(int index) {
-        return Doors.get(index);
-    }
-
-    public List<Door> getDoors() {
-        return Doors;
-    }
-
-    public void addBot(Bot newBot) {
-        Bots.add(newBot);
-    }
-
-    public void getBot(int index) {
-        Bots.get(index);
-    }
-
-    public List<Bot> getBots() {
-        return Bots;
-    }
-
-    public Bot findBot(String BotName, int UserPos) throws Exception {
-        Bot tempBot;
-
-        for (int i = 0; i < Bots.size(); i++) {
-            tempBot = Bots.get(i);
-            
-            if (tempBot.getName().equals(BotName) && tempBot.getPosition() == UserPos) {
-                return tempBot;
-            }
-
-        }
-        throw new Exception("I can't find " + BotName + " !");
-    }
-
-    public Door findDoorToGo(int userRoom, Location goLocation) {
-        Door tempDoor;
-
-        for (int i = 0; i < Doors.size(); i++) {
-            tempDoor = Doors.get(i);
-            
-            if (tempDoor.getRoomA() == userRoom && tempDoor.getPosA() == goLocation) {
-                return tempDoor;
-            } else if (tempDoor.getRoomB() == userRoom && tempDoor.getPosB() == goLocation) {
-                return tempDoor;
-            }
-        }
-
-        return null;
-
-    }
-
-    public boolean findDoorToUse(int userRoom, ITool Tool) {
-        Door tempDoor;
-        for (int i = 0; i < Doors.size(); i++) {
-            tempDoor = Doors.get(i);
-            if (tempDoor.getRoomA() == userRoom || tempDoor.getRoomB() == userRoom) {
-                if (!tempDoor.isIsOpen()) {
-                    if (Tool.getToolValue() == tempDoor.getIdPass()) {
-                        tempDoor.unlockDoor();
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
 }
--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Room.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/enviroment/Room.java
@@ -5,7 +5,10 @@
  */
 package io.github.dmmn_games.earth_2045.enviroment;
 
+import io.github.dmmn_games.earth_2045.doors.Door;
+import io.github.dmmn_games.earth_2045.game.Location;
 import io.github.dmmn_games.earth_2045.items.*;
+import io.github.dmmn_games.earth_2045.npcs.Bot;
 import io.github.dmmn_games.earth_2045.npcs.Enemy;
 import io.github.dmmn_games.earth_2045.tools.*;
 import java.io.Serializable;
@@ -17,9 +20,12 @@ import java.util.*;
  */
 public class Room implements Serializable {
 
+    private List<Door> Doors = new ArrayList<>();
+    private List<Bot> Bots = new ArrayList<>();
     private List<ITool> tools = new ArrayList<>();
     private List<IItem> items = new ArrayList<>();
     private List<Enemy> enemies = new ArrayList<>();
+    
 
     public Room() {
         this.tools = new ArrayList<>();
@@ -76,4 +82,80 @@ public class Room implements Serializable {
         
         throw new Exception("I can't find " + Name + " !!!");
     }
+
+    public void addDoor(Door newDoor) {
+        Doors.add(newDoor);
+    }
+
+
+    public Door getDoor(int index) {
+        return Doors.get(index);
+    }
+
+    public void getBot(int index) {
+        Bots.get(index);
+    }
+
+    public List<Bot> getBots() {
+        return Bots;
+    }
+
+    public List<Door> getDoors() {
+        return Doors;
+    }
+
+    public void addBot(Bot newBot) {
+        Bots.add(newBot);
+    }
+
+    
+    
+    public Bot findBot(String BotName, int UserPos) throws Exception {
+        Bot tempBot;
+
+        for (int i = 0; i < Bots.size(); i++) {
+            tempBot = Bots.get(i);
+            
+            if (tempBot.getName().equals(BotName) && tempBot.getPosition() == UserPos) {
+                return tempBot;
+            }
+
+        }
+        throw new Exception("I can't find " + BotName + " !");
+    }
+    
+    /*
+    public boolean findDoorToUse(int userRoom, ITool Tool) {
+        Door tempDoor;
+        for (int i = 0; i < Doors.size(); i++) {
+            tempDoor = Doors.get(i);
+            if (tempDoor.getRoomA() == userRoom || tempDoor.getRoomB() == userRoom) {
+                if (!tempDoor.isIsOpen()) {
+                    if (Tool.getToolValue() == tempDoor.getIdPass()) {
+                        tempDoor.unlockDoor();
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+    
+     public Door findDoorToGo(int userRoom, Location goLocation) {
+        Door tempDoor;
+
+        for (int i = 0; i < Doors.size(); i++) {
+            tempDoor = Doors.get(i);
+            
+            if (tempDoor.getRoomA() == userRoom && tempDoor.getPosA() == goLocation) {
+                return tempDoor;
+            } else if (tempDoor.getRoomB() == userRoom && tempDoor.getPosB() == goLocation) {
+                return tempDoor;
+            }
+        }
+
+        return null;
+
+    }*/
 }
