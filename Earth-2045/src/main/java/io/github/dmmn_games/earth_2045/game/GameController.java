8939d5e6b1e5e781e8850e4ba32c5ff7b8f92a48
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-24 16:31:44 +0300

    initial world

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/game/GameController.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/game/GameController.java
@@ -6,8 +6,10 @@
 package io.github.dmmn_games.earth_2045.game;
 
 import io.github.dmmn_games.earth_2045.commands.CommandsController;
+import io.github.dmmn_games.earth_2045.doors.Door;
 import io.github.dmmn_games.earth_2045.elevator.Elevator;
 import io.github.dmmn_games.earth_2045.enviroment.*;
+import static io.github.dmmn_games.earth_2045.game.Location.WEST;
 import io.github.dmmn_games.earth_2045.user.User;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,27 +21,41 @@ import java.util.List;
 public class GameController implements java.io.Serializable {
 
     private CommandsController CommandsController;
-    private List<String> CommandHistory;
-
     private User User;
-    private List<Floor> Floors;
-    private Elevator Elevator;
 
-    public Elevator getElevator() {
-        return Elevator;
-    }
 
     public GameController() {
         CommandsController = new CommandsController();
-
-        this.CommandHistory = new ArrayList<>();
-
-        Floors = new ArrayList<>();
-
         initWorld();
     }
 
     private void initWorld() {
+        Floor flrGround= new Floor();
+        Floor flr0= new Floor();
+        Floor flr1= new Floor();
+        Floor flr2= new Floor();
+        Floor flr3= new Floor();
+        Floor flr4= new Floor();
+        
+        flrGround.linkfloors(null, flr1);
+        flr0.linkfloors(flrGround, flr1);
+        flr1.linkfloors(flr0, flr2);
+        flr2.linkfloors(flr1, flr3);
+        flr3.linkfloors(flr2,flr4);
+        flr4.linkfloors(flr3, null);
+        
+//        Room rm1= new Room();
+//        Room rm2= new Room();
+//        Room rm3= new Room();
+//        Room rm4= new Room();
+
+        flr1.addRoom(new Room());
+        flr1.addRoom(new Room());
+        flr1.addRoom(new Room());
+        flr1.addRoom(new Room());
+        
+        flr1.getRoom(1).addDoor(new Door("aa",1,WEST,flr1.getRoom(2),true));
+        
         
     }
 
@@ -51,13 +67,6 @@ public class GameController implements java.io.Serializable {
         this.CommandsController = CommandsController;
     }
 
-    public List<String> getCommandHistory() {
-        return CommandHistory;
-    }
-
-    public void setCommandHistory(ArrayList<String> CommandHistory) {
-        this.CommandHistory = CommandHistory;
-    }
 
     public User getUser() {
         return User;
@@ -66,17 +75,4 @@ public class GameController implements java.io.Serializable {
     public void setUser(User User) {
         this.User = User;
     }
-
-    public List<Floor> getFloors() {
-        return Floors;
-    }
-
-    public Floor getFloor(int Index) {
-        return this.Floors.get(Index);
-    }
-
-    public void setFloors(List<Floor> Floors) {
-        this.Floors = Floors;
-    }
-
 }
