7855d09f42df81c0dc93b58c2baa1da6b50d31f6
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-31 15:16:23 +0300

    modify

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/game/GameController.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/game/GameController.java
@@ -12,7 +12,7 @@ import static io.github.dmmn_games.earth_2045.game.Location.*;
 import io.github.dmmn_games.earth_2045.npcs.Enemy;
 import io.github.dmmn_games.earth_2045.tools.*;
 import io.github.dmmn_games.earth_2045.user.User;
-import java.util.Random;
+import java.util.*;
 
 /**
  *
@@ -22,46 +22,34 @@ public class GameController implements java.io.Serializable {
 
     private CommandsController CommandsController;
     private User User;
-
-    private Floor flrGround;
-    private Floor flr0;
-    private Floor flr1;
-    private Floor flr2;
-    private Floor flr3;
-    private Floor flr4;
+     private List<Floor> floors;
 
     private int time;
     private int nextTrigger;
 
     public GameController() {
         CommandsController = new CommandsController();
+        floors=new ArrayList<>();
     }
-
+    
+     public void addFloor(Floor newFloor) {
+        floors.add(newFloor);
+    }
     public void initWorld(String Username, int time) {
         this.time = time;
         this.nextTrigger = time - 30;
-
-        // Create floors
-        flrGround = new Floor();
-        flr0 = new Floor();
-        flr1 = new Floor();
-        flr2 = new Floor();
-        flr3 = new Floor();
-        flr4 = new Floor();
-
-        // Link floors
-        flrGround.linkfloors(null, flr1);
-        flr0.linkfloors(flrGround, flr1);
-        flr1.linkfloors(flr0, flr2);
-        flr2.linkfloors(flr1, flr3);
-        flr3.linkfloors(flr2, flr4);
-        flr4.linkfloors(flr3, null);
-
+        
+        floors.addFloor(new Floor());
+        floors.addFloor(new Floor());
+        floors.addFloor(new Floor());
+        floors.addFloor(new Floor());
+        floors.addFloor(new Floor());
+        
         // Add rooms on flr1
-        flr1.addRoom(new Room());
-        flr1.addRoom(new Room());
-        flr1.addRoom(new Room());
-        flr1.addRoom(new Room());
+        floors.get(1).addRoom(new Room());
+        floors.get(1).addRoom(new Room());
+        floors.get(1).addRoom(new Room());
+        floors.get(1).addRoom(new Room());
 
         // Add content for the floor1
         flr1.getRoom(0).addDoor(new Door("door0to1", 100, NORTH, flr1.getRoom(1), false));
