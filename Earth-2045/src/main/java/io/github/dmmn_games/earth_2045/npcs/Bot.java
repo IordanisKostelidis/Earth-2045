10affb4c22123ef91c1334b8505d87c2ba32d3f2
elgreko <elgreko@Lenovo-PC>
2017-06-01 20:26:16 +0300

    bot and bot conversation

--- Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/npcs/Bot.java
+++ Earth-2045/src/main/java/io/github/dmmn_games/earth_2045/npcs/Bot.java
@@ -85,13 +85,7 @@ public class Bot implements  Serializable {
 
     }
 
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public int getPosition() {
-        return position;
-    }
+    
 
     public String getQuestion()
     {
